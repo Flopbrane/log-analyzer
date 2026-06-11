78662439520fcec1201798bff9c53ae38d9b357c
cam_f <cam_farm65@yahoo.co.jp>
2026-06-11 09:15:58 +0900

    add:"experimental flatten_dict.py"

--- utils/flatten_dict.py
+++ utils/flatten_dict.py
@@ -7,6 +7,7 @@
 #########################
 from __future__ import annotations
 
+from collections.abc import Mapping
 from typing import Any, cast
 
 __all__: list[str] = [
@@ -17,19 +18,35 @@ __all__: list[str] = [
 
 
 def flatten_dict(
-    data: dict[str, Any],
+    data: Mapping[str, Any],
     prefix: str = "",
 ) -> dict[str, Any]:
-    """ネストしたdictをドット区切りへ展開する。"""
+    """
+    ネストした辞書をドット区切りの
+    フラットな辞書へ変換する。
+
+    例:
+        {
+            "what": {
+                "message": "error"
+            }
+        }
+
+    ↓
+
+        {
+            "what.message": "error"
+        }
+    """
 
     result: dict[str, Any] = {}
 
     for key, value in data.items():
         new_key: str = key if not prefix else f"{prefix}.{key}"
 
-        if isinstance(value, dict):
-            child: dict[str, Any] = cast(
-                dict[str, Any],
+        if isinstance(value, Mapping):
+            child: Mapping[str, Any] = cast(
+                Mapping[str, Any],
                 value,
             )
 
@@ -39,7 +56,51 @@ def flatten_dict(
                     new_key,
                 )
             )
+        elif isinstance(value, list):
+            result[new_key] = flatten_list(cast(list[Any], value))
         else:
             result[new_key] = value
+
+    return result
+
+
+def flatten_list(
+    values: list[Any],
+) -> list[Any]:
+    """
+    ネストしたリストをフラットなリストへ変換する。
+
+    例:
+        [
+            {
+                "message": "error"
+            },
+            [
+                {
+                    "message": "warning"
+                }
+            ],
+        ]
+
+    ↓
+
+        [
+            {
+                "message": "error"
+            },
+            {
+                "message": "warning"
+            },
+        ]
+    """
+    result: list[Any] = []
+
+    for value in values:
+        if isinstance(value, list):
+            result.extend(flatten_list(cast(list[Any], value)))
+        elif isinstance(value, Mapping):
+            result.extend(flatten_dict(cast(Mapping[str, Any], value)).values())
+        else:
+            result.append(value)
 
     return result
